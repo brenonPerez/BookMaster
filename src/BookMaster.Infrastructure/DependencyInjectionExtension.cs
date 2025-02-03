6a93806803f2265408ff8c5a66990ce34fb16a92
Brenon Tavares <brenonperez1997@gmail.com>
2025-02-03 16:01:37 -0300

    feat: add DependencyInjectionExtension para configurar serviços e repositórios

---
+++ src/BookMaster.Infrastructure/DependencyInjectionExtension.cs
@@ -0,0 +1,31 @@
+﻿using BookMaster.Domain.Repositories.Books;
+using BookMaster.Infrastructure.DataAccess;
+using BookMaster.Infrastructure.DataAccess.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BookMaster.Infrastructure;
+public static class DependencyInjectionExtension
+{
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext(configuration);
+        services.AddRepositories();
+    }
+
+    private static void AddDbContext(this IServiceCollection services, IConfiguration configuration)
+    {
+
+        var connectionString = configuration.GetConnectionString("Connection");
+        var serverVersion = new MySqlServerVersion(new Version(8, 0, 40));
+
+        services.AddDbContext<BookMasterDbContext>(config => config.UseMySql(connectionString, serverVersion));
+    }
+
+    private static void AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IBooksOnlyRepository, BooksOnlyRepository>();
+    }
+}
+
