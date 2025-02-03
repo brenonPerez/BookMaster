7b020083b50f38f2a4ba52a67c1ed380ce7640e3
Brenon Tavares <brenonperez1997@gmail.com>
2025-02-03 16:41:10 -0300

    feat: add DependencyInjectionExtension para configurar serviços da application

--- src/BookMaster.Api/Program.cs
+++ src/BookMaster.Api/Program.cs
@@ -1,4 +1,5 @@
 using BookMaster.Infrastructure;
+using BookMaster.Application;
 using BookMaster.Infrastructure.Migrations;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplication();
 
 var app = builder.Build();
 
---
+++ src/BookMaster.Application/DependencyInjectionExtension.cs
@@ -0,0 +1,16 @@
+﻿using BookMaster.Application.UseCases.Book.Register;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BookMaster.Application;
+public static class DependencyInjectionExtension
+{
+    public static void AddApplication(this IServiceCollection services)
+    {
+        services.AddUseCases();
+    }
+
+    private static void AddUseCases(this IServiceCollection services)
+    {
+        services.AddScoped<IRegisterBookUseCase, RegisterBookUseCase>();
+    }
+}
