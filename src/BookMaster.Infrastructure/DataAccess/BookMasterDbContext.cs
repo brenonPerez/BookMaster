3e43cf0594d98eed674f4d9475a2f67cd19a9f64
Brenon Tavares <brenonperez1997@gmail.com>
2025-02-03 15:50:33 -0300

    feat: add BookMasterDbContext para gerenciamento de dados

---
+++ src/BookMaster.Infrastructure/DataAccess/BookMasterDbContext.cs
@@ -0,0 +1,12 @@
+﻿using BookMaster.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookMaster.Infrastructure.DataAccess;
+internal class BookMasterDbContext : DbContext
+{
+    public BookMasterDbContext(DbContextOptions<BookMasterDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Book> Books { get; set; }
+}
