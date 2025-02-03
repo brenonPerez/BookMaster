b948d3101d2b9249b4b7d8ea5c7cf70449a51409
Brenon Tavares <brenonperez1997@gmail.com>
2025-02-03 14:57:37 -0300

    feat: add entidade Book ao domínio BookMaster

---
+++ src/BookMaster.Domain/Entities/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace BookMaster.Domain.Entities;
+
+public class Book
+{
+    public long Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Author { get; set; } = string.Empty;
+    public string Publisher { get; set; } = string.Empty;
+    public int Year { get; set; }
+    public string Summary { get; set; } = string.Empty;
+}
