6e99b1abef3589b01d5ecb25c473e4376e6885fe
Brenon Tavares <brenonperez1997@gmail.com>
2025-02-03 15:53:57 -0300

    feat: add BooksOnlyRepository para adicionar livros

---
+++ src/BookMaster.Infrastructure/DataAccess/Repositories/BooksOnlyRepository.cs
@@ -0,0 +1,18 @@
+﻿using BookMaster.Domain.Entities;
+using BookMaster.Domain.Repositories.Books;
+
+namespace BookMaster.Infrastructure.DataAccess.Repositories;
+internal class BooksOnlyRepository : IBooksOnlyRepository
+{
+    private readonly BookMasterDbContext _dbContext;
+    public BooksOnlyRepository(BookMasterDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task Add(Book book)
+    {
+        await _dbContext.Books.AddAsync(book);
+        await _dbContext.SaveChangesAsync();
+    }
+}
