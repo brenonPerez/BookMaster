6fce3065b9bb083b2764ac146b98713ab387fff5
Brenon Tavares <brenonperez1997@gmail.com>
2025-02-03 15:12:10 -0300

    feat: add BooksController com endpoint de registro de livros

---
+++ src/BookMaster.Api/Controllers/BooksController.cs
@@ -0,0 +1,21 @@
+﻿using BookMaster.Application.UseCases.Book.Register;
+using BookMaster.Communication.Requests;
+using BookMaster.Communication.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookMaster.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class BooksController : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseBookJson), StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Register(
+        [FromBody] RequestBookJson request,
+        [FromServices] IRegisterBookUseCase useCase)
+    {
+        var response = await useCase.Execute(request);
+        return CreatedAtAction(string.Empty, response);
+    }
+}
