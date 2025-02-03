d334472a1992c3b978277b5db5999f936c7ebf14
Brenon Tavares <brenonperez1997@gmail.com>
2025-02-03 15:10:38 -0300

    feat: add interface IRegisterBookUseCase para registrar livros

---
+++ src/BookMaster.Application/UseCases/Book/Register/IRegisterBookUseCase.cs
@@ -0,0 +1,8 @@
+﻿using BookMaster.Communication.Requests;
+using BookMaster.Communication.Responses;
+
+namespace BookMaster.Application.UseCases.Book.Register;
+public interface IRegisterBookUseCase
+{
+    Task<ResponseBookJson> Execute(RequestBookJson request);
+}
