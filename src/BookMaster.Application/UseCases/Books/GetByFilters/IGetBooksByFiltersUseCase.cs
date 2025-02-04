0889741dc3d084ecb6eeb426d2e3588bb670433a
Brenon Tavares <brenonperez1997@gmail.com>
2025-02-04 01:14:00 -0300

    feat: add IGetBooksByFiltersUseCase para obter livros por filtros

---
+++ src/BookMaster.Application/UseCases/Books/GetByFilters/IGetBooksByFiltersUseCase.cs
@@ -0,0 +1,7 @@
+﻿using BookMaster.Communication.Responses;
+
+namespace BookMaster.Application.UseCases.Books.GetByFilters;
+public interface IGetBooksByFiltersUseCase
+{
+    Task<ResponseBooksJson> Execute(string? title, string? author, string? publisher);
+}
