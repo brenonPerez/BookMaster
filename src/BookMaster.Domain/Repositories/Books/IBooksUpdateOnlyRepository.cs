d9ae7a836a1998a03166cd0b2aa289e7d08c403d
Brenon Tavares <brenonperez1997@gmail.com>
2025-02-03 23:48:28 -0300

    feat: add IBooksUpdateOnlyRepository para atualização de livros

---
+++ src/BookMaster.Domain/Repositories/Books/IBooksUpdateOnlyRepository.cs
@@ -0,0 +1,8 @@
+﻿using BookMaster.Domain.Entities;
+
+namespace BookMaster.Domain.Repositories.Books;
+public interface IBooksUpdateOnlyRepository
+{
+    Task<Book?> GetById(long  id);
+    void Update(Book book);
+}
