1e58b39133bcf74f65fa3ff4480ab6e29d260901
Brenon Tavares <brenonperez1997@gmail.com>
2025-02-03 21:34:50 -0300

    feat: add interface IGetBookByIdUseCase para buscar um livro pelo id

---
+++ src/BookMaster.Application/UseCases/Book/GetById/IGetBookByIdUseCase.cs
@@ -0,0 +1,7 @@
+﻿using BookMaster.Communication.Responses;
+
+namespace BookMaster.Application.UseCases.Book.GetById;
+public interface IGetBookByIdUseCase
+{
+    Task<ResponseBookJson> Execute(long id);
+}
