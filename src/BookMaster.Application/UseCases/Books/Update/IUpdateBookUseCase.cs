c51f2c1a5eee47d0754c7644d6b65300730bb170
Brenon Tavares <brenonperez1997@gmail.com>
2025-02-03 23:43:27 -0300

    feat: add IUpdateBookUseCase para atualizar livros

---
+++ src/BookMaster.Application/UseCases/Books/Update/IUpdateBookUseCase.cs
@@ -0,0 +1,7 @@
+﻿using BookMaster.Communication.Requests;
+
+namespace BookMaster.Application.UseCases.Books.Update;
+public interface IUpdateBookUseCase
+{
+    Task Execute(long id, RequestBookJson request);
+}
