f2016c1c5c8a2d98ced8be9db052d7974a0eb22e
Brenon Tavares <brenonperez1997@gmail.com>
2025-02-03 23:10:09 -0300

    feat: add NotFoundException para tratamento de exceções de não encontrado

---
+++ src/BookMaster.Exception/ExceptionBase/NotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace BookMaster.Exception.ExceptionBase;
+public class NotFoundException : BookMasterException
+{
+    public NotFoundException(string message) : base(message) { }
+
+    public override int StatusCode => (int)HttpStatusCode.BadRequest;
+
+    public override List<string> GetErrors()
+    {
+        return [Message];
+    }
+}
